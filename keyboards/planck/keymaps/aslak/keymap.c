e86498df01d4a664e6413f25037b8ff7b2573359
ats1995 <assi_assy@hotmail.com>
2019-08-04 18:33:12 +0200

    RSFT on home and F3 on GAME layer

--- keyboards/planck/keymaps/aslak/keymap.c
+++ keyboards/planck/keymaps/aslak/keymap.c
@@ -1,12 +1,11 @@
-#include "planck.h"
-
+#include "planck.h" 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 
 // Fillers to make layering more clear
-#define PREVENT_STUCk_MODIFIERS
+#define PREVENT_STUCK_MODIFIERS
 
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
@@ -26,13 +25,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_LBRC},
   {KC_ESC,    KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT},
   {KC_BSPC,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_ENT},
-  {MO(_FN),   KC_RALT,   KC_LCTRL,  KC_LGUI,   KC_LSFT,   KC_SPC,    KC_SPC,    MO(_RS),   MO(_LW),   KC_LALT, KC_NO,/*RSF*/DF(_GAME)}
+  {MO(_FN),   KC_RALT,   KC_LCTRL,  KC_LGUI,   KC_LSFT,   KC_SPC,    KC_SPC,    MO(_RS),   MO(_LW),   KC_LALT,   KC_RSFT,   DF(_GAME)}
 },
 [_GAME] = { // Game
   {KC_1,      KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P},
   {KC_2,      KC_ESC,    KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN},
   {KC_3,      KC_6,      KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH},
-  {KC_4,      KC_5,      KC_LCTL,   KC_LALT,   KC_LSFT,   KC_SPC,    KC_SPC,    KC_F5,     MO(_LW),   KC_ENT,    KC_ENT,    DF(_QW)}
+  {KC_4,      KC_5,      KC_LCTL,   KC_LALT,   KC_LSFT,   KC_SPC,    KC_SPC,    KC_F5,     MO(_LW),   KC_ENT,    KC_F3,     DF(_QW)}
 },
 [_RS] = { /* RAISE, symbols */
   {S(KC_GRV), S(KC_1),   S(KC_2),   S(KC_3),   S(KC_4),   S(KC_5),   S(KC_6),   S(KC_7),   S(KC_8),   S(KC_9),   S(KC_0),   S(KC_MINUS)},
@@ -44,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_BSLASH},
   {KC_ESC,    KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT},
   {KC_BSPC,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_ENT},
-  {MO(_FN),   KC_RALT,   KC_LCTRL,  KC_LGUI,   KC_LSFT,   KC_SPC,    KC_SPC,    MO(_US_RS),MO(_LW),   KC_LALT,   KC_TRNS,   DF(_GAME)}
+  {MO(_FN),   KC_RALT,   KC_LCTRL,  KC_LGUI,   KC_LSFT,   KC_SPC,    KC_SPC,    MO(_US_RS),MO(_LW),   KC_LALT,   KC_RSFT,   DF(_GAME)}
 },
 [_US_RS] = { /* US RAISE, symbols */
   {S(KC_GRV), S(KC_1),   S(KC_2),   S(KC_3),    S(KC_4),  S(KC_5),   S(KC_6),   S(KC_7),  S(KC_8),    S(KC_9),   S(KC_0),   S(KC_MINUS)},
@@ -54,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 },
 [_LW] = { /* LOWER, navigation */
   {KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,    KC_F9,      KC_F10,    KC_F11,    KC_F12},
-  {KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_LEFT,   KC_DOWN,  KC_UP,      KC_RGHT,   KC_APP,    KC_NO},
+  {KC_TRNS,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_LEFT,   KC_DOWN,  KC_UP,      KC_RGHT,   KC_APP,    KC_NO},
   {KC_INS,    KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_HOME,   KC_PGDN,  KC_PGUP,    KC_END,    KC_PSCR,   KC_SLCK},
   {KC_CAPS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_LSFT,   KC_SPC,    KC_SPC,    KC_TRNS,  KC_TRNS,    KC_LALT,   KC_NO,     KC_PAUS}
 },
@@ -97,9 +96,3 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
   }
   return MACRO_NONE;
 };
-
-/* TODO
-
--KC_TRNS -> KC_NO
-
-*/
