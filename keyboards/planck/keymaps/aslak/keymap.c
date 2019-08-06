27d1daf83937369da0803c020c2b1ede5471f22a
ats1995 <assi_assy@hotmail.com>
2019-08-06 21:00:58 +0200

    initial working setup

--- keyboards/planck/keymaps/aslak/keymap.c
+++ keyboards/planck/keymaps/aslak/keymap.c
@@ -15,14 +15,16 @@
  */
 
 #include QMK_KEYBOARD_H
-#include "muse.h"
+//#include "muse.h"
 
 extern keymap_config_t keymap_config;
 
 enum planck_layers {
   _QWERTY,
+  _GAME,
   _LOWER,
   _RAISE,
+  _FN,
   _ADJUST
 };
 
@@ -32,7 +34,8 @@ enum planck_keycodes {
   DVORAK,
   BACKLIT,
   EXT_PLV,
-  TEST
+  TEST,
+  KVM1
 };
 
 #define LOWER MO(_LOWER)
@@ -44,11 +47,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_LBRC,
     KC_ESC,    KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,
     KC_BSPC,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_ENT,
-    KC_NO,     KC_RALT,   KC_LCTRL,  KC_LGUI,   KC_LSFT,   KC_SPC,    KC_SPC,    _RAISE,    _LOWER,    KC_LALT,   KC_RSFT,   KC_NO // 1st MO(_FN), last DF(_GAME)
+    MO(_FN),   KC_RALT,   KC_LCTRL,  KC_LGUI,   KC_LSFT,   KC_SPC,    KC_SPC,    RAISE,     LOWER,     KC_LALT,   KC_RSFT,   DF(_GAME)
 ),
 [_LOWER] = LAYOUT_planck_grid(
     KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,    KC_F9,      KC_F10,    KC_F11,    KC_F12,
-    KC_TRNS,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_LEFT,   KC_DOWN,  KC_UP,      KC_RGHT,   KC_APP,    KC_NO,
+    KC_TRNS,   KVM1,      KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_LEFT,   KC_DOWN,  KC_UP,      KC_RGHT,   KC_APP,    KC_NO,
     KC_INS,    KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_HOME,   KC_PGDN,  KC_PGUP,    KC_END,    KC_PSCR,   KC_SLCK,
     KC_CAPS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_LSFT,   KC_SPC,    KC_SPC,    KC_TRNS,  KC_TRNS,    KC_LALT,   KC_NO,     KC_PAUS
 
@@ -59,9 +62,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_DEL,    KC_NUBS,   RALT(KC_2),RALT(KC_3),RALT(KC_4),KC_EQL, RALT(KC_RBRC),RALT(KC_7),RALT(KC_8),RALT(KC_9),RALT(KC_0),KC_BSLASH,
     KC_TRNS,   S(KC_RBRC),KC_TRNS,   KC_TRNS,   KC_LSFT,   KC_SPC,    KC_SPC,    KC_TRNS,   KC_TRNS,   KC_LALT,   KC_TRNS,   KC_TRNS
 ),
+[_GAME] = LAYOUT_planck_grid(
+    KC_1,      KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,
+    KC_2,      KC_ESC,    KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,
+    KC_3,      KC_6,      KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,
+    KC_4,      KC_5,      KC_LCTL,   KC_LALT,   KC_LSFT,   KC_SPC,    KC_SPC,    KC_F5,     KC_F9,     KC_ENT,    KC_F3,     DF(_QWERTY)
+),
+[_FN] = LAYOUT_planck_grid(
+    KC_VOLU,   KC_TRNS,   KC_MS_U,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,  _______,    _______,   KC_TRNS,   DEBUG,
+    KC_MPLY,   KC_MS_L,   KC_MS_D,   KC_MS_R,   KC_TRNS,   KC_NO,     KC_WH_L,   KC_WH_D,  KC_WH_U,    KC_WH_R,   KC_TRNS,   KC_TRNS,
+    KC_VOLD,   KC_MPRV,   KC_MNXT,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,
+    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_MS_BTN2,KC_MS_BTN1,KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,   _______
+),
 [_ADJUST] = LAYOUT_planck_grid(
     _______,   RESET,     DEBUG,     RGB_TOG,   RGB_MOD,   RGB_HUI,   RGB_HUD,   RGB_SAI,   RGB_SAD,    RGB_VAI,   RGB_VAD,   KC_DEL ,
-    _______,   _______,   MU_MOD,    AU_ON,     AU_OFF,    AG_NORM,   AG_SWAP,   QWERTY,    COLEMAK,    DVORAK,    _______,    _______,
+    _______,   _______,   MU_MOD,    AU_ON,     AU_OFF,    AG_NORM,   AG_SWAP,   QWERTY,    COLEMAK,    DVORAK,    _______,   _______,
     _______,   MUV_DE,    MUV_IN,    MU_ON,     MU_OFF,    MI_ON,     MI_OFF,    TERM_ON,   TERM_OFF,   _______,   _______,   _______,
     _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,    _______,   _______,   _______
 )
@@ -87,6 +102,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
       break;
     case TEST:
+      if (record->event.pressed) {
+        SEND_STRING("TeSt");
+      }
+      break;
+    case KVM1:
       if (record->event.pressed) {
         SEND_STRING("TeSt");
       }
